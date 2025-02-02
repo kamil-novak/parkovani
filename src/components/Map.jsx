6285185adb32a34bf16e73a922c4947342777f56
Kamil Novák <kamil.novak86@gmail.com>
2025-02-02 20:20:05 +0100

    Focus na Search

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -335,6 +335,7 @@ function Map(props) {
       searchWidgetRef.current.domNode.querySelector(".esri-search__submit-button")?.addEventListener("click", () => {
         if (!searchWidgetRef.current.domNode.classList.contains("search-expanded")) {
           searchWidgetRef.current.domNode.classList.add("search-expanded")
+          searchWidgetRef.current.domNode.querySelector("input").focus()
         }
       });
 
